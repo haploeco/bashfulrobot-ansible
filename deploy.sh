277b1df4537cc9bb580bff5e904e3db64b06b75b
bashfulrobot <dustin@bashfulrobot.com>
2018-07-30 23:03:55 -0700

    Added Ansible bootstrap to deploy.sh. Need a better way.

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,15 @@
 
 APULL=$(which ansible-pull)
 MYREPO="https://github.com/bashfulrobot/bashfulrobot-ansible.git"
+MYPPA="ansible"
+
+# Bootstrap Ansible
+if ! grep -q "$MYPPA" /etc/apt/sources.list.d/*; then
+    echo "Ansible Repo not available"
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install ansible
+fi
 
 sudo $APULL -U $MYREPO
