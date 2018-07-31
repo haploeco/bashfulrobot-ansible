fb51ebfc77e3456f297b8aa3a3bee108dece9b2f
bashfulrobot <dustin@bashfulrobot.com>
2018-07-31 07:50:07 -0700

    Updated deploy.sh with "-y" while installing Ansible. Nuke Libreoffice APT pkg. Added irccloud, hugo, libreoffice, remmina, spotiy, lxd, telegram, go, restic, snapcraft snap packages.

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ if ! grep -q "$MYPPA" /etc/apt/sources.list.d/*; then
     sudo apt-get install software-properties-common
     sudo apt-add-repository ppa:ansible/ansible
     sudo apt-get update
-    sudo apt-get install ansible
+    sudo apt-get install ansible -y
 fi
 
 sudo $APULL -U $MYREPO
