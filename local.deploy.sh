37c58768a70a93ed52aa4443ff6201e2e9c6cc75
Dustin Krysak <dustin@bashfulrobot.com>
2018-10-16 14:46:59 -0700

    Add local deploy script to iterate changes locally prior to check in on github.

---
+++ local.deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+APULL=$(which ansible-pull)
+MYREPO="$HOME/tmp/bashfulrobot-ansible/."
+MYPPA="ansible"
+
+# Bootstrap Ansible
+if ! grep -q "$MYPPA" /etc/apt/sources.list.d/*; then
+    echo "Ansible Repo not available"
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install git ansible -y
+fi
+
+
+if [ ! -f $HOME/.ansible.cfg ]; then
+    touch $HOME/.ansible.cfg
+    echo '[defaults]' > $HOME/.ansible.cfg
+    echo 'remote_tmp     = /tmp/$USER/ansible' >> $HOME/.ansible.cfg
+fi
+
+sudo $APULL -U $MYREPO
