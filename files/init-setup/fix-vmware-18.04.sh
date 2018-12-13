9dfc1384c4249cd816f6815929bd4935584de1a9
Dustin Krysak <dustin@bashfulrobot.com>
2018-12-13 10:00:32 -0800

    Added VMWare fix init script - Still need to integrate into ansible itself.

---
+++ files/init-setup/fix-vmware-18.04.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo ln -s /usr/lib/x86_64-linux-gnu/libgstreamer-1.0.so.0.1400.0 /usr/lib/x86_64-linux-gnu/libgstreamer-0.10.so.0
+
+sudo ln -s /usr/lib/x86_64-linux-gnu/libgstapp-1.0.so.0 /usr/lib/x86_64-linux-gnu/libgstapp-0.10.so.0
+
+sudo ln -s /usr/lib/x86_64-linux-gnu/libgstbase-1.0.so.0.1400.0 /usr/lib/x86_64-linux-gnu/libgstbase-0.10.so.0
+
+exit 0
