31416e80a0b1bb8630af7bcf8973e8b1e12c9376
Dustin Krysak <bashfulrobot@ubuntu.com>
2019-02-01 14:28:17 -0800

    zenburn theme added

--- files/dotfiles/fish/config.fish
+++ files/dotfiles/fish/config.fish
@@ -29,7 +29,9 @@ set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 set -g theme_show_exit_status yes
 # set -g default_user your_normal_user
-set -g theme_color_scheme dark
+# Themes listed here:
+# https://github.com/oh-my-fish/theme-bobthefish
+set -g theme_color_scheme zenburn
 # set -g fish_prompt_pwd_dir_length 0
 # set -g theme_project_dir_length 1
 
