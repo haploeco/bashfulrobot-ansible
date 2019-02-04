21d4dcd1b3ab7831fc1f143e82f65658de58dd48
Dustin Krysak <bashfulrobot@ubuntu.com>
2019-02-04 14:25:06 -0800

    Enable vi keybindings on the CLI.

--- files/dotfiles/fish/config.fish
+++ files/dotfiles/fish/config.fish
@@ -35,6 +35,9 @@ set -g theme_color_scheme zenburn
 # set -g fish_prompt_pwd_dir_length 0
 # set -g theme_project_dir_length 1
 
+# Set vi keybindings to be closer to bash
+fish_vi_key_bindings
+
 # enable emoji in shell
 # why? why not.
 set -U -x fish_emoji_width 2
