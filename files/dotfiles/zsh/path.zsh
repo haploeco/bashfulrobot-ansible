6e27851efd05d3042145572154edfe8541943371
bashfulrobot <dustin@bashfulrobot.com>
2019-05-21 16:57:30 -0700

    Added path.zsh
    Setup the structure to have a template in place to add $PATH modifications. Leverages the oh-my-zsh custom folder.

---
+++ files/dotfiles/zsh/path.zsh
@@ -0,0 +1,7 @@
+# You can put files here to add functionality separated per file, which
+# will be ignored by git.
+# Files on the custom/ directory will be automatically loaded by the init
+# script, in alphabetical order.
+
+# Add example bin
+#export PATH=/path/to/bin:$PATH
