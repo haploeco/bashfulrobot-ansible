52118a72bc0a21cbb6ec2549ae9569629ffc04c6
bashfulrobot <dustin@bashfulrobot.com>
2019-05-21 12:11:23 -0700

    Added asciinema curl upload function.
    Work around for asciinema bug on 19.04 - https://github.com/asciinema/asciinema/issues/353

--- files/dotfiles/zsh/functions.zsh
+++ files/dotfiles/zsh/functions.zsh
@@ -2,3 +2,7 @@
 # will be ignored by git.
 # Files on the custom/ directory will be automatically loaded by the init
 # script, in alphabetical order.
+
+function asciinema-upload() {
+curl -v -u $USER:$(cat ~/.config/asciinema/install-id) https://asciinema.org/api/asciicasts -F asciicast=@$1
+}
