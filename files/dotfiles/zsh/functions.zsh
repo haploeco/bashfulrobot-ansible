48389054c2e73a0aa401705d5fa6cf8fb512da19
bashfulrobot <dustin@bashfulrobot.com>
2019-05-22 09:30:23 -0700

    Added Update Functions
    Created a wrapper function that performs multiple APT commands to keep my system in good health. Also broke out a few reusable functions that can be used in other functions, etc.

--- files/dotfiles/zsh/functions.zsh
+++ files/dotfiles/zsh/functions.zsh
@@ -6,3 +6,21 @@
 function asciinema-upload() {
 curl -v -u $USER:$(cat ~/.config/asciinema/install-id) https://asciinema.org/api/asciicasts -F asciicast=@$1
 }
+
+function do-update() {
+  echoHeader "Starting System Update"
+  echoSection "Updating Sources"
+  sudo apt update
+  sleep 1
+  echoSection "Updating Distro"
+  sudo apt dist-upgrade -y
+  sleep 1
+  echoSection "Performing Autoremove"
+  sudo apt clean -y
+  sudo apt autoremove -y
+  sleep 1
+  echoSection "Adding Missing Deps"
+  sudo apt install -f -y
+  sleep 1
+  echoHeader "Completed System Update"
+}
---
+++ files/dotfiles/zsh/util-functions.zsh
@@ -0,0 +1,28 @@
+# These utilities are used in multiple
+# zsh functions on my system
+
+# Vars
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+NC=$(tput sgr0) 
+
+ONLINE="${GREEN}online$NC"
+OFFLINE="${RED}offline$NC"
+
+
+function echoHeader () {
+  echo
+  echo
+  printf "%40s \n" "[$RED $1 $NC]"
+  # echo "> > >  $1 =========="
+  echo
+}
+
+function echoSection () {
+  echo
+  echo
+  printf "$GREEN > > > $NC %1s \n" "$YELLOW $1 $NC"
+  # echo "> > >  $1 =========="
+  echo
+}
